6a62a55571e25c137e2b60e408d6619e3275dc74
brucezhu27 <brucezhu27@163.com>
2018-07-30 16:22:35 +0800

    spring 入门实例

---
+++ chapter3/src/main/java/com/smart/web/BbsDaemon.java
@@ -0,0 +1,23 @@
+package com.smart.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Spring boot 入门测试.
+ */
+@RestController
+@EnableAutoConfiguration
+public class BbsDaemon {
+
+//    @RequestMapping("/")
+//    public String index() {
+//        return "欢迎来到傲来国!";
+//    }
+//
+//    public static void main(String[] args) throws Exception{
+//        SpringApplication.run(BbsDaemon.class, args);
+//    }
+}
